3db6361bb73c9d2e8387513cf934a484a777dff9
Bardia Naderi <bardia.naderi@gmail.com>
2025-02-08 16:24:41 -0500

    refactoring cities

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,26 @@
 import { useState } from 'react';
 import { WeatherCard } from './components/Weather';
 import { CitySelector } from './components/CitySelector';
+import { cities } from './constants/cities';
 import './App.css';
 
-const cities = [
-  {
-    id: 6167865,
-    name: "Toronto",
-    country: "CA"
-  },
-  {
-    id: 6094817,
-    name: "Ottawa",
-    country: "CA"
-  },
-  {
-    id: 1850147,
-    name: "Tokyo",
-    country: "JP"
-  }
-];
 
 function App() {
-  const [selectedCity, setSelectedCity] = useState(null);
+ const [selectedCity, setSelectedCity] = useState(null);
 
-  return (
-    <div className="app">
-      <h1>Weather App</h1>
-        <CitySelector 
-          cities={cities} 
-          selectedCity={selectedCity} 
-          onSelectCity={setSelectedCity} 
-        />
-        {selectedCity && <WeatherCard city={selectedCity} />}
-    </div>
-  );
+
+ return (
+   <div className="app">
+     <h1>Weather App</h1>
+     <CitySelector
+       cities={cities}
+       selectedCity={selectedCity}
+       onSelectCity={setSelectedCity}
+     />
+     {selectedCity && <WeatherCard city={selectedCity} />}
+   </div>
+ );
 }
+
 
 export default App;
